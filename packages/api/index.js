717f9f1a56c02c16231374774f77ff957340c418
zevdvlpr <zevdvlpr@gmail.com>
2020-11-02 16:51:41 -0300

    :gear: perf: remove bot

--- packages/api/index.js
+++ packages/api/index.js
@@ -2,4 +2,4 @@ require('dotenv/config');
 
 require('./app').listen(3000, () => console.log('Server is booming on port 3000'));
 
-require('./client').login(process.env.TOKEN, () => console.log('Bot online'));
+// require('./client').login(process.env.TOKEN, () => console.log('Bot online'));
