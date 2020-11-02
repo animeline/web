714961e9a1f67314fed9e8eebff936e7b58d5451
zevdvlpr <zevdvlpr@gmail.com>
2020-11-02 16:42:25 -0300

    :bulb: chore: bot login

--- packages/api/index.js
+++ packages/api/index.js
@@ -1,8 +1,14 @@
+require('dotenv/config');
+
 const express = require("express");
+const { Client } = require('discord.js');
+
 const app = express();
 
-app.get('/',(req,res) => {
-  res.send("This is a sample express app");
-});
+app.get('/',(req,res) => res.send("This is a sample express app"));
+app.listen(3000, () => console.log('Server is booming on port 3000'));
 
-app.listen(3000, () => console.log('Server is booming on port 5000'));
+const client = new Client();
+
+client.on('ready', () => client.user.setPresence({ activity: { name: 'Bot online' } }));
+client.login(process.env.TOKEN, () => console.log('Bot online'));
