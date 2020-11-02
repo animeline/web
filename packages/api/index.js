a468e92a23641604d44d77d5827279e03523e863
zevdvlpr <zevdvlpr@gmail.com>
2020-11-02 16:42:56 -0300

    :bulb: chore: client

--- packages/api/index.js
+++ packages/api/index.js
@@ -4,11 +4,10 @@ const express = require("express");
 const { Client } = require('discord.js');
 
 const app = express();
-
-app.get('/',(req,res) => res.send("This is a sample express app"));
-app.listen(3000, () => console.log('Server is booming on port 3000'));
-
 const client = new Client();
 
+app.get('/',(req,res) => res.send("This is a sample express app"));
 client.on('ready', () => client.user.setPresence({ activity: { name: 'Bot online' } }));
+
 client.login(process.env.TOKEN, () => console.log('Bot online'));
+app.listen(3000, () => console.log('Server is booming on port 3000'));
