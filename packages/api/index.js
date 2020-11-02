099bffbc0a0ef85e9c3499887078ba2b04ff1c62
zevdvlpr <zevdvlpr@gmail.com>
2020-11-02 16:48:22 -0300

    :gear: perf: import app and bot

--- packages/api/index.js
+++ packages/api/index.js
@@ -1,13 +1,5 @@
 require('dotenv/config');
 
-const express = require("express");
-const { Client } = require('discord.js');
+require('./app').listen(3000, () => console.log('Server is booming on port 3000'));
 
-const app = express();
-const client = new Client();
-
-app.get('/',(req,res) => res.send("This is a sample express app"));
-client.on('ready', () => client.user.setPresence({ activity: { name: 'Bot online' } }));
-
-client.login(process.env.TOKEN, () => console.log('Bot online'));
-app.listen(3000, () => console.log('Server is booming on port 3000'));
+require('./client').login(process.env.TOKEN, () => console.log('Bot online'));
