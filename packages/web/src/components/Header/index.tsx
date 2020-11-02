bef22f8f56a1d62c547a15602382cb68dd1970cf
zevdvlpr <zevdvlpr@gmail.com>
2020-11-02 15:53:37 -0300

    :bulb: chore: components

---
+++ packages/web/src/components/Header/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import {
+  Container,
+  LogoArea,
+  HamburgerIcon,
+  Logo,
+  SearchContainer,
+  SearchBar,
+  IconContainer,
+  SearchIcon,
+  UserArea,
+  UserAvatar,
+} from './styles';
+
+const Header: React.FC = () => {
+  return (
+    <Container>
+      <LogoArea>
+        <HamburgerIcon />
+
+        <Logo>
+          Animes<span>Line</span>
+        </Logo>
+      </LogoArea>
+
+      <SearchContainer>
+        <SearchBar placeholder="Procurar conteúdo" />
+
+        <IconContainer>
+          <SearchIcon />
+        </IconContainer>
+      </SearchContainer>
+
+      <UserArea>
+        <UserAvatar />
+      </UserArea>
+    </Container>
+  );
+};
+
+export default Header;
