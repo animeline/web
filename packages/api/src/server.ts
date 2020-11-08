cd845e1209e642c2b770dc037ca69fd1d233be46
zevdvlpr <zevdvlpr@gmail.com>
2020-11-08 15:22:46 -0300

    :gear: perf: dotenv config

--- packages/api/src/server.ts
+++ packages/api/src/server.ts
@@ -1,10 +1,20 @@
-import "dotenv/config";
 import "reflect-metadata";
+
+import path from "path";
+
+import dotenv from "dotenv";
+
+dotenv.config({
+  path:
+    process.env.NODE_ENV === "development"
+      ? path.join(__dirname, "..", ".env.development")
+      : path.join(__dirname, "..", ".env.production"),
+});
 
 import AppManager from "./core/managers/API";
 
 const appManager = new AppManager({
-  port: 3333,
+  port: process.env.PORT,
 });
 
 appManager.connect();
