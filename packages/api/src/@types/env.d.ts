195f3a676c727fc679b9cd9d36409f07bdd5d4cd
zevdvlpr <zevdvlpr@gmail.com>
2020-11-19 23:02:38 -0300

    :beetle: fix: remove

--- packages/api/src/@types/env.d.ts
+++
@@ -1,8 +0,0 @@
-declare namespace NodeJS {
-  interface ProcessEnv {
-    PORT: number;
-    NODE_ENV: 'production' | 'development';
-
-    ANIME_API_URL: string;
-  }
-}
--- packages/api/src/app.ts
+++
@@ -1,7 +0,0 @@
-import express from "express";
-
-const app = express().disable("x-powered-by");;
-
-app.use(express.json())
-
-export default app;
