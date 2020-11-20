436346354f942185288eeea18e328dfc6b1ed66e
zevdvlpr <zevdvlpr@gmail.com>
2020-11-19 22:56:28 -0300

    :bulb: chore: utils

---
+++ packages/api/src/shared/utils/path.ts
@@ -0,0 +1,12 @@
+  
+import { join } from 'path';
+
+const baseUrl = join(__dirname, '..', '..');
+
+export function getPath(...paths: string[]): string {
+  return join(...paths);
+}
+
+export function getRootPath(...paths: string[]): string {
+  return join(baseUrl, ...paths);
+}
