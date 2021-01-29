780f3e2cd67bcb24783edfa5bfd705240c576330
zevdvlpr <zevdvlpr@gmail.com>
2021-01-29 18:05:15 -0300

    💡 chore: import header

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,10 +3,14 @@ import { Route, Router, Switch } from 'react-router-dom';
 
 import { Home } from '@pages/Home';
 
+import { Header } from '@components/Header';
+
 import { history } from './history';
 
 export const Routes: React.FC = () => (
   <Router history={history}>
+    <Header />
+
     <Switch>
       <Route path="/" exact component={Home} />
     </Switch>
