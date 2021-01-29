081b241a8ebaead471c794bf38fad717f3bf8be5
zevdvlpr <zevdvlpr@gmail.com>
2021-01-29 14:09:34 -0300

    💡 chore: styling typing

---
+++ src/@types/styled.d.ts
@@ -0,0 +1,11 @@
+import 'styled-componets';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    primary: string;
+    secondary: string;
+    background: string;
+    white: string;
+    black: string;
+  }
+}
